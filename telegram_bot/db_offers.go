6b29b43425227125f8b4c5e221bfce6b55e2e66c
nikolaisilaev <nikolaisilaev@yandex-team.ru>
2023-12-10 01:37:33 +0300

    ..

--- telegram_bot/db_offers.go
+++ telegram_bot/db_offers.go
@@ -2,15 +2,21 @@ package telegram_bot
 
 import (
 	"github.com/gofrs/uuid"
+	"sync"
 )
 
 var OfferIdGenerator = uuid.DefaultGenerator
 var OffersHistory []StocksOffer
+var mutex sync.Mutex // Mutex to protect OffersHistory
 
 func SaveOffer(offer StocksOffer) {
+	mutex.Lock() // Lock before modifying OffersHistory
 	OffersHistory = append(OffersHistory, offer)
+	mutex.Unlock() // Unlock after modifying
 }
 
 func LoadOffers() []StocksOffer {
+	mutex.Lock()         // Lock before accessing OffersHistory
+	defer mutex.Unlock() // Unlock after accessing (defer ensures that the unlock will happen)
 	return OffersHistory
 }
